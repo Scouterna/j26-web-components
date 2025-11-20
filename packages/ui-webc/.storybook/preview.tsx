c8085ab8164eebfa0633e6f6118bd1bf65576c47
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-11-20 22:53:20 +0100

    chore: remove old storybook stuff

--- packages/ui-webc/.storybook/preview.tsx
+++
@@ -1,9 +0,0 @@
-import "../src/global/global.css";
-
-// import { defineCustomElements } from "../loader/index.js";
-
-// /**
-//  * Registers all custom elements in the Storybook preview.
-//  * This is useful if your components rely on other nested Stencil components.
-//  */
-// defineCustomElements();
