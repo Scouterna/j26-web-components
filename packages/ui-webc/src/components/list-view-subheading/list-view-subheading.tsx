1a62fe46e1bc399cf334822cd78ac2ab2e7ee6b2
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-12-15 20:57:38 +0100

    fix: use proper heading

--- packages/ui-webc/src/components/list-view-subheading/list-view-subheading.tsx
+++ packages/ui-webc/src/components/list-view-subheading/list-view-subheading.tsx
@@ -9,8 +9,13 @@ import { Component, Host, h, Prop } from "@stencil/core";
 })
 export class ScoutListViewSubheading {
   @Prop() text: string;
+  @Prop() headingLevel: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" = "h2";
 
   render() {
-    return <Host role="listitem">{this.text}</Host>;
+    return (
+      <Host role="listitem">
+        <this.headingLevel class="heading">{this.text}</this.headingLevel>
+      </Host>
+    );
   }
 }
